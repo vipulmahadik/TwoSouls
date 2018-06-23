5a159ee91c27eef220ce5e5cad02efc7e956254d
Vipulkumar Mahadik <vipul.mahadik@gmail.com>
2018-06-23 16:05:52 -0700

    layout added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,70 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+import { Container, Row, Col } from 'reactstrap';
 import './App.less';
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
+            <div className="TwoSouls">
+                <div className="app-page bg-color-gray">
+                    <div className="center-wrapper">
+                        <Container>
+                            <Row>
+                                <Col>Invitation</Col>
+                                <Col>Picture</Col>
+                            </Row>
+                        </Container>
+                    </div>
+                </div>
+                <div className="app-page">
+                    <div className="center-wrapper">
+                        <Container>
+                            <Row>
+                                <Col>About Venue</Col>
+                            </Row>
+                            <Row>
+                                <Col>Pre Wedding Party</Col>
+                                <Col>Wedding Ceremony</Col>
+                                <Col>Reception</Col>
+                            </Row>
+                        </Container>
+                    </div>
+                </div>
+                <div className="app-page bg-color-gray">
+                    <div className="center-wrapper">
+                        <Container>
+                            <Row>
+                                <Col>About Bride and Groom</Col>
+                            </Row>
+                            <Row>
+                                <Col sm="6">Bride Picture</Col>
+                                <Col sm="6">Bride Info</Col>
+                                <Col sm="6">Groom Info</Col>
+                                <Col sm="6">Groom Picture</Col>
+                            </Row>
+                        </Container>
+                    </div>
+                </div>
+                <div className="app-page">
+                    <div className="center-wrapper">
+                        <Container>
+                            <Row>
+                                <Col>Message from Groom's parents</Col>
+                            </Row>
+                            <Row>
+                                <Col sm="6">Groom's Parents Message</Col>
+                                <Col sm="6">Groom's Parents Picture</Col>
+                            </Row>
+                            <Row>
+                                <Col>Message from Bride's parents</Col>
+                            </Row>
+                            <Row>
+                                <Col sm="6">Bride's Parents Picture</Col>
+                                <Col sm="6">Bride's Parents Message</Col>
+                            </Row>
+                        </Container>
+                    </div>
+                </div>
             </div>
         );
     }
